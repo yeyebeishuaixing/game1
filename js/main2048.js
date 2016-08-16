6039367e5278372b0835448a83228083faabb46a
yeyebeishuaixing <longtl93111@126.com>
2016-08-16 18:03:26 +0800

    Add files via upload

--- js/main2048.js
+++ js/main2048.js
@@ -166,11 +166,13 @@ $(document).keydown( function( event ){
 });
 
 document.addEventListener('touchstart',function(event){
+    event.preventDefault();
     startx = event.touches[0].pageX;
     starty = event.touches[0].pageY;
 });
 
 document.addEventListener('touchend',function(event){
+    event.preventDefault();
     endx = event.changedTouches[0].pageX;
     endy = event.changedTouches[0].pageY;
 
